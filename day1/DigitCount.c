c9cd82b31e9db6db52bf20e9c25c69f417556b88
aniruddh1543 <aniruddh1543@gmail.com>
2026-06-01 14:33:16 +0530

    day1 uploaded

---
+++ day1/DigitCount.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int count=0,i,n;
+printf("enter the number : ");
+scanf("%d",&n);
+while (n>0)
+{
+    n=n/10;
+    count++;
+}
+    printf("Total digits = %d", count);
+return 0;
+
+
+
+}  
---
+++ day1/Factorial.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int fac=1,i,n;
+printf("enter the number : ");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{ 
+    
+    fac=fac*i;
+
+
+}
+
+printf("factorial of a %d is %d",n,fac);
+
+
+} 
